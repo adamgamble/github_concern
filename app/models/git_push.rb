cf5f200dfdeb4cbfbadb09a3cfea2b9aba46013e
Adam Gamble <adamgamble@gmail.com>
2011-12-05 20:45:10 -0600

    both repo and branch keys work

--- app/models/git_push.rb
+++ app/models/git_push.rb
@@ -16,20 +16,21 @@ class GitPush < ActiveRecord::Base
 
   def associate_git_concernables
     GithubConcern::Engine.github_concerns.each_pair do |class_name, github_concern_hash|
+      bucket = class_name.constantize
       github_concern_hash.each_pair do |github_concern_key, github_concern_method|
         case github_concern_key
         when :repo
-          Rails.logger.warn "Looking for repos on #{class_name}"
-          repo_name = payload["repository"]["name"]
-          Rails.logger.warn "Running method find_all_by_#{github_concern_method} on #{class_name} with argument #{repo_name}"
-          objects = class_name.constantize.send("find_all_by_#{github_concern_method}".to_sym,repo_name)
-          Rails.logger.warn "Found #{objects.count}"
-          objects.each do |object|
-            github_concernable_git_push = self.github_concernable_git_pushes.new
-            github_concernable_git_push.github_concernable = object
-            github_concernable_git_push.save
-          end
+          git_variable = payload["repository"]["name"]
+        when :branch
+          git_variable = payload["ref"].gsub("refs/heads/","")
         end
+        bucket = bucket.where(github_concern_method => git_variable)
+      end
+      bucket.all.each do |object|
+        github_concernable_git_push = self.github_concernable_git_pushes.new
+        github_concernable_git_push.github_concernable = object
+        github_concernable_git_push.save
+        object.github_concern_callback if object.respond_to?(:github_concern_callback)
       end
     end
   end
